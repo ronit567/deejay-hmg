801db9a456ce3b3910304975cde5498861ca67ba
Ronit Longia <ronitlongia@dyn172-22-29-1.reznet.wireless.uwo.pri>
2025-02-11 14:19:58 -0500

    a

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename='/deejay-hmg/' > 
+  <BrowserRouter basename = '/deejay-hmg/' > 
   <StrictMode>
     <App />
   </StrictMode>
