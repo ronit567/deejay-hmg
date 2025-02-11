823409b611b0f4e0c36cb74e3ee537c6c64ab66a
Ronit Longia <ronitlongia@dyn172-22-29-1.reznet.wireless.uwo.pri>
2025-02-11 13:56:57 -0500

    a

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/deejay-hmg/",  
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react'],  
   },
 });
